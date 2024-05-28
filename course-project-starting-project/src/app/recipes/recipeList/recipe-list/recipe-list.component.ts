7982a1f60ae7bdce0928b8cb32418d2eb341a31c
SW-Muriu <sw.muriu@gmail.com>
2024-05-28 10:28:29 +0300

    Recipe list configured to loop through all the recipes in the recipe's array

--- course-project-starting-project/src/app/recipes/recipeList/recipe-list/recipe-list.component.ts
+++ course-project-starting-project/src/app/recipes/recipeList/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../recipes.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("A test Title", "A recipe Descroption", 'https://wallpaperaccess.com/full/3691432.jpg'),
+    new Recipe("A test Title", "A recipe Descroption", 'https://wallpaperaccess.com/full/3691432.jpg')
 
+  ];
+  constructor(){}
 }
--- course-project-starting-project/src/app/recipes/recipes.component.ts
+++ course-project-starting-project/src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './recipes.module';
 
 @Component({
   selector: 'app-recipes',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class RecipesComponent {
 
+ 
+
+
+  constructor() { }
 }
