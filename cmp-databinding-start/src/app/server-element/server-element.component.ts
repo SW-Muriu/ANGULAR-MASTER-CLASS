ef1423a864942d33205f9cbe0340d232c445884d
SW-Muriu <sw.muriu@gmail.com>
2024-06-03 11:58:59 +0300

    View Encapsulation and DOM styling

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 
 
@@ -6,7 +6,13 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css'
+  styleUrl: './server-element.component.css', 
+
+  //Global style without local encapsulation
+  // encapsulation: ViewEncapsulation.None
+
+  //For Native, it uses the traditional dom structuring
+  // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ServerElementComponent implements OnInit {
 
