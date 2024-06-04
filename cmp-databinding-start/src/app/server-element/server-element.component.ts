a49d6322f41ea7edbce93add83770200c9141e75
SW-Muriu <sw.muriu@gmail.com>
2024-06-04 12:21:05 +0300

    Content Child and Viewchild

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,5 +1,5 @@
 import { getLocaleFirstDayOfWeek } from '@angular/common';
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 
 
@@ -7,7 +7,7 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css', 
+  styleUrl: './server-element.component.css',
 
   //Global style without local encapsulation
   // encapsulation: ViewEncapsulation.None
@@ -16,12 +16,14 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
   // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,
-AfterContentChecked, AfterViewInit, AfterViewChecked  {
+  AfterContentChecked, AfterViewInit, AfterViewChecked {
 
   //Assigning an alias to the input decorator 
   @Input('srvElement') element: { name: string, type: string, content: string }
 
   @Input() name: string;
+
+  @ViewChild('heading') header: ElementRef;
 
 
 
@@ -35,7 +37,6 @@ AfterContentChecked, AfterViewInit, AfterViewChecked  {
   //The only hook that takes in an argument
   ngOnChanges(changes: SimpleChanges) {
     console.log('Ng on changes called', changes);
-    
   }
 
   ngDoCheck() {
@@ -44,6 +45,7 @@ AfterContentChecked, AfterViewInit, AfterViewChecked  {
 
   ngOnInit(): void {
     console.log('NgOnInit Called');
+    
   }
 
   ngAfterContentInit() {
@@ -57,6 +59,7 @@ AfterContentChecked, AfterViewInit, AfterViewChecked  {
 
   ngAfterViewInit() {
     console.log("Ng after view init called");
+    console.log("Text Content: ", this.header.nativeElement.textContent);
   }
 
 
@@ -66,6 +69,6 @@ AfterContentChecked, AfterViewInit, AfterViewChecked  {
 
   ngOnDestroy() {
     console.log("Ng on destroy called");
-    
+
   }
 }
