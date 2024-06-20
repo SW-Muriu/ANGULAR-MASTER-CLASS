09ad567a02580a0d9046c129249a0cfa8f14c7d0
SW-Muriu <sw.muriu@gmail.com>
2024-06-20 08:45:51 +0300

    Adopted activated route for component class programatic navigation

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLoadServers(): void{
+
+    //Using absolute paths. Check relative paths in the servers component
+    const route = '/servers';
+    this.router.navigate([route]);
   }
 
 }
